321b06e7156a76757b6ae48db9f7bbfc910910d8
Michael Stallings <stall84@gmail.com>
2022-04-08 18:24:46 -0400

    Finished coding out the depthFirstPrint function with comments

--- prac1.ts
+++ prac1.ts
@@ -1,21 +1,37 @@
 /**
- * @description depthFirstPrint funciton will print out the nodes in the order they are traversed using DFT.
- * @param graph The graph of all nodes to be input
+ * @description depthFirstPrint funciton will print out the nodes in 
+ *              the order they are traversed using DFT.
+ * @param graph The graph as an adjacency list (object in JS)
  * @param origin The starting node
  */
 
 const depthFirstPrint = (graph: any, origin: string): void => {
-  const stack = [];
-  stack.push(origin);
-  // TODO: Finish
-  // console.log('test DepthFirstPrint');
+  const stack = [ origin ];       // Initialize our stack with the origin node
+  for (let iter = 0; stack.length > 0; iter++) {
+    // A while loop could also be used here. We want to print-out the currently traversed node. 
+    // In a DFT, that will be the Last-In node. The last node in an array (our stack) can be pop()'d off the top
+    const currentNode = stack.pop();
+    console.log(currentNode)    // Good practice to assign a definite variable for the element you're popping off to print/log
+    if (currentNode) {
+      for (let neigbhor of graph[currentNode]) {
+        stack.push(neigbhor);
+      }
+    } 
+  }
 }
 
+/**
+ * @description An Adjacency List representation of our graph
+ * @param key Node/Vertex
+ * @param value Neighbor Nodes/Vertices of key, respecting directionality.
+ */
 const graph = {
-  a: ["b", "c"],
+  a: ["c", "b"],
   b: ["d"],
   c: ["e"],
-  d: ["f"],
-  e: [],
-  f: [],
+  d: [],
+  e: ["b"],
+  f: ["d"],
 };
+
+depthFirstPrint(graph, 'a')
