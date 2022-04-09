02ab921ed237f28f8c11dc4ef969cd610b84f8e9
Michael Stallings <stall84@gmail.com>
2022-04-08 22:21:26 -0400

    Finished a much-longer-than-should-have-taken excursion into a recursive alternate to the iterative approach of the first depthFirstPrint function

--- prac1.ts
+++ prac1.ts
@@ -1,11 +1,25 @@
 /**
- * @description depthFirstPrint funciton will print out the nodes in 
- *              the order they are traversed using DFT.
+ * @description An Adjacency List representation of our graph
+ * @param key Node/Vertex
+ * @param value Neighbor Nodes/Vertices of key, respecting directionality.
+ */
+ const graph = {
+  a: ["c", "b"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+};
+
+/**
+ * @description depthFirstPrintIterative funciton will print out the nodes in 
+ *              the order they are traversed using DFT with an iterative looping function.
  * @param graph The graph as an adjacency list (object in JS)
  * @param origin The starting node
  */
 
-const depthFirstPrint = (graph: any, origin: string): void => {
+const depthFirstPrintIterative = (graph: any, origin: string): void => {
   const stack = [ origin ];       // Initialize our stack with the origin node
   for (let iter = 0; stack.length > 0; iter++) {
     // A while loop could also be used here. We want to print-out the currently traversed node. 
@@ -21,17 +35,25 @@ const depthFirstPrint = (graph: any, origin: string): void => {
 }
 
 /**
- * @description An Adjacency List representation of our graph
- * @param key Node/Vertex
- * @param value Neighbor Nodes/Vertices of key, respecting directionality.
+ * @description depthFirstPrintRecursive funciton will print out the nodes in 
+ *              the order they are traversed using DFT with a recursive function.
+ * @param graph The graph as an adjacency list (object in JS)
+ * @param currOrigin The starting node which will double as the current node for this recursion 
  */
-const graph = {
-  a: ["c", "b"],
-  b: ["d"],
-  c: ["e"],
-  d: [],
-  e: ["b"],
-  f: ["d"],
-};
 
-depthFirstPrint(graph, 'a')
+const depthFirstPrintRecursive = (graph: any, currOrigin: string): void => {
+  // The origin is again the first node we want to print. Instead of forming a stack array and iterating. Go ahead and have the
+  // function do the printing immediately of what will be the origin, but what will also become the current Node when it's called on itself later
+  console.log(currOrigin);
+  // Since the graph adjacensy list is using arrays to store the neighbor nodes, loop over them.
+  for (let neighbor of graph[currOrigin]) {
+    depthFirstPrintRecursive(graph, neighbor)
+  }
+  // TODO: Figure out why the basic for-loop below wouldn't work at the 'd' node.
+  // for (let neighborator = 0; graph[currOrigin].length > 0; neighborator++) {
+  //   depthFirstPrintRecursive(graph, graph[currOrigin][0]);
+  // }
+}
+
+// depthFirstPrintIterative(graph, 'a')
+depthFirstPrintRecursive(graph, 'a')
